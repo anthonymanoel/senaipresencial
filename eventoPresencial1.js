05b905c3bc1b7dc688b83446f4930d7372d2b662
Anthony Manoel <72582172+anthonymanoel@users.noreply.github.com>
2021-09-20 13:47:59 -0300

    Add files via upload

---
+++ eventoPresencial1.js
@@ -0,0 +1,23 @@
+{
+    // Verificar se a data colocada é atual ou não
+console.log("digite o dia de hoje")
+    var diaDeHoje=20;
+var mesHoje=09;    
+    if(diaDeHoje=20)
+    console.log("digite o mês");
+    else if(mesHoje=09);
+    console.log("data inserida corretamente, prossiga");
+} // Código para verificar idade, se for maior que 18 pode entrar, menor de idade não
+ const idade=18 
+ console.log("digite sua idade, só aceitamos maiores de "+ idade + " anos") 
+ if(idade >= 18);
+ console.log("Idade permitida");
+
+// Código para inserir lista de participantes com limite máximo de 100 pessoas
+// https://www.youtube.com/watch?v=gcFHdujqzd0
+{
+    const texto = 'xxx'
+    for (let i =''; i.length.toString().length != texto.length; i+= 'x') {
+        console.log(i.length + 'x'.length)
+    } 
+}
